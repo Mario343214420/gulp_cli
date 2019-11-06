95551398157fec8fe5364187ea886a05112b8f0b
Mario <44432410+Mario343214420@users.noreply.github.com>
2019-11-06 15:49:35 +0800

    Add files via upload

---
+++ dist/js/test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var sum = function sum(a, b) {
+  return a + b + 2;
+};
+var a = 1;
+var b = 2;
+var c = sum(3, 5);
+console.log(a, b, c);
+console.log(a);
---
+++ src/js/test.js
@@ -0,0 +1,8 @@
+let sum = function (a, b) {
+  return a + b + 2
+}
+let a = 1
+let b = 2
+let c=sum(3,5)
+console.log (a,b,c);
+console.log (a);
